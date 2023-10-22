aaa0d4577cf7a9f569b60811224e96a1e4a45e3f
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-23 04:53:25 +0700

    Create main.js

---
+++ camera/js/main.js
@@ -0,0 +1,44 @@
+var webkam = {
+  
+  worker : null, // tesseract worker
+  hVid : null, hGo :null, hRes : null, // html elements
+  init : () => {
+    
+    webkam.hVid = document.getElementById("vid"),
+    webkam.hGo = document.getElementById("go"),
+    webkam.hRes = document.getElementById("result");
+
+    
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(async (stream) => {
+      
+      webkam.worker = await Tesseract.createWorker();
+      await webkam.worker.loadLanguage("eng");
+      await webkam.worker.initialize("eng");
+
+      
+      webkam.hVid.srcObject = stream;
+      webkam.hGo.onclick = webkam.snap;
+    })
+    .catch(err => console.error(err));
+  },
+
+  // (B) SNAP VIDEO FRAME TO TEXT
+  snap : async () => {
+    // (B1) CREATE NEW CANVAS
+    let canvas = document.createElement("canvas"),
+        ctx = canvas.getContext("2d"),
+        vWidth = webkam.hVid.videoWidth,
+        vHeight = webkam.hVid.videoHeight;
+  
+    // (B2) CAPTURE VIDEO FRAME TO CANVAS
+    canvas.width = vWidth;
+    canvas.height = vHeight;
+    ctx.drawImage(webkam.hVid, 0, 0, vWidth, vHeight);
+
+    // (B3) CANVAS TO IMAGE, IMAGE TO TEXT
+    const res = await webkam.worker.recognize(canvas.toDataURL("image/png"));
+    webkam.hRes.value = res.data.text;
+  },
+};
+window.addEventListener("load", webkam.init);
