436c4237fc1ac116d326c168e0c121c9af4413be
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-23 10:49:01 +0700

    Update main.js

--- camera/js/main.js
+++ camera/js/main.js
@@ -47,27 +47,24 @@ var webkam = {
       await webkam.worker.loadLanguage("eng");
       await webkam.worker.initialize("eng");
 
-      
       webkam.hVid.srcObject = stream;
       webkam.hGo.onclick = webkam.snap;
     })
     .catch(err => console.error(err));
   },
 
-  // (B) SNAP VIDEO FRAME TO TEXT
   snap : async () => {
-    // (B1) CREATE NEW CANVAS
     let canvas = document.createElement("canvas"),
-        ctx = canvas.getContext("2d"),
-        vWidth = webkam.hVid.videoWidth,
-        vHeight = webkam.hVid.videoHeight;
-  
-    // (B2) CAPTURE VIDEO FRAME TO CANVAS
+    ctx = canvas.getContext("2d"),
+    vWidth = webkam.hVid.videoWidth,
+    vHeight = webkam.hVid.videoHeight;
+
     canvas.width = vWidth;
     canvas.height = vHeight;
     ctx.drawImage(webkam.hVid, 0, 0, vWidth, vHeight);
 
-    // (B3) CANVAS TO IMAGE, IMAGE TO TEXT
+    webkam.hVid.stop();
+
     const res = await webkam.worker.recognize(canvas.toDataURL("image/png"));
     webkam.hRes.value = res.data.text;
   },
