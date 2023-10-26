18316939139934f3ebc36a3d38abdd849ba9e7aa
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-26 11:41:02 +0700

    Update main.js
    Signed-off-by: CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>

--- message/js/main.js
+++ message/js/main.js
@@ -2,7 +2,7 @@ let url = 'https://docs.google.com/spreadsheets/d/1ex4zW0WxE3dP7UhtqNrhxY7ufvTWE
 const lkjs = document.querySelector('.lkjs');
 const query = encodeURIComponent('Select B,C,D');
 var coll = document.getElementsByClassName("collapsible");
-var i;
+var trump;
 console.log(query);
 url = url + '&tq=' + query;
 
@@ -11,6 +11,17 @@ fetch(url)
 .then(rep => {
     const data = JSON.parse(rep.substr(47).slice(0,-2));
     const container = document.createElement('div');
+    for (trump = 0; trump < coll.length; trump++) {
+        coll[trump].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        });
+    }
     data.table.rows.forEach((main)=>{
         lkjs.append(container);
         console.log(main.c[0]);
@@ -42,17 +53,6 @@ fetch(url)
         container.appendChild(mContent);
         mContent.appendChild(mParaf1);
         mContent.appendChild(mParaf2);
-        for (i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-              this.classList.toggle("active");
-              var content = this.nextElementSibling;
-              if (content.style.maxHeight){
-                content.style.maxHeight = null;
-              } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-                } 
-            });
-        }
     });
     console.log(data);
 });
