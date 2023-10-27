e19be47a76f4f8a8d9dc5424f2441af8ab0dfa3e
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-27 12:30:51 +0700

    Create index.js
    Signed-off-by: CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>

---
+++ core/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDUlPDeA-GOPoW5TFap6IZauyNrpBB3Vfo",
+  authDomain: "cusmedroid-team-1.firebaseapp.com",
+  databaseURL: "https://cusmedroid-team-1-default-rtdb.firebaseio.com",
+  projectId: "cusmedroid-team-1",
+  storageBucket: "cusmedroid-team-1.appspot.com",
+  messagingSenderId: "788667487315",
+  appId: "1:788667487315:web:6693ca84b90ba0e099e2a6",
+  measurementId: "G-WCLWY678WP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
