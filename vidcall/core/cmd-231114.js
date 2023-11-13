c62cbfa2518a169ad30412b2c63e2362141bee38
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-11-14 04:49:38 +0700

    Add files via upload
    Signed-off-by: CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>

---
+++ vidcall/core/cmd-231114.js
@@ -0,0 +1,57 @@
+//Create an account on Firebase, and use the credentials they give you in place of the following
+var config = {
+  apiKey: "AIzaSyDUlPDeA-GOPoW5TFap6IZauyNrpBB3Vfo",
+  authDomain: "cusmedroid-team-1.firebaseapp.com",
+  databaseURL: "https://cusmedroid-team-1-default-rtdb.firebaseio.com",
+  projectId: "cusmedroid-team-1",
+  storageBucket: "cusmedroid-team-1.appspot.com",
+  messagingSenderId: "788667487315",
+  appId: "1:788667487315:web:6693ca84b90ba0e099e2a6",
+  measurementId: "G-WCLWY678WP"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database().ref();
+var yourVideo = document.getElementById("yourVideo");
+var friendsVideo = document.getElementById("friendsVideo");
+var yourId = Math.floor(Math.random()*1000000000);
+//Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'beaver','username': 'webrtc.websitebeaver@gmail.com'}]};
+var pc = new RTCPeerConnection(servers);
+pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+
+function sendMessage(senderId, data) {
+    var msg = database.push({ sender: senderId, message: data });
+    msg.remove();
+}
+
+function readMessage(data) {
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    if (sender != yourId) {
+        if (msg.ice != undefined)
+            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        else if (msg.sdp.type == "offer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+              .then(() => pc.createAnswer())
+              .then(answer => pc.setLocalDescription(answer))
+              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+        else if (msg.sdp.type == "answer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+    }
+};
+
+database.on('child_added', readMessage);
+
+function showMyFace() {
+  navigator.mediaDevices.getUserMedia({audio:true, video:true})
+    .then(stream => yourVideo.srcObject = stream)
+    .then(stream => pc.addStream(stream));
+}
+
+function showFriendsFace() {
+  pc.createOffer()
+    .then(offer => pc.setLocalDescription(offer) )
+    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+}
